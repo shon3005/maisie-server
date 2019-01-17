2260553c7a80a2c9925b5d7594ac7082a03165ed
Shaun Chua <shaunc@vikartech.com>
2019-01-17 18:50:25 -0500

    added comment

--- maisie_site/shared/components/socialButtons.js
+++ maisie_site/shared/components/socialButtons.js
@@ -1,5 +1,5 @@
 const socials = [["instagram", "https://instagram.com/sayheymaisie"], ["twitter", "https://twitter.com/sayheymaisie"], ["facebook", "https://www.facebook.com/pg/Maisie-2274087959477864/"]]
-
+// 
 export default () => socials.map((x, index) =>
     <a key={x} className="socialbuttons__button" href={x[1]}>
       <div className="col-c-c">
