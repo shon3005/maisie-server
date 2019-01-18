1f8adaa40fe4094f98db45631de25343a4a59896
mkochakian <mattk4@me.com>
2019-01-17 23:29:22 -0500

    discover page

--- maisie_site/modules/discover/index.js
+++ maisie_site/modules/discover/index.js
@@ -1,5 +1,9 @@
 export default () =>
   <div className="discover col-fs-c">
+    <div className="discover__toprow row-sb-c">
+      <div className="col-c">Welcome back!</div>
+      <button>message</button>
+    </div>
     <div className="discover__cta col-c-c">
       <img src="../../static/landing/circle_large.svg" />
       <div>Join a new Circle</div>
