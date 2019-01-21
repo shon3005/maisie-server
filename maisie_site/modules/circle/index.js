19a4179ec85d7322cfae33a415b739977c3a286b
mkochakian <mattk4@me.com>
2019-01-21 14:15:50 -0500

    building out circle page

--- maisie_site/modules/circle/index.js
+++ maisie_site/modules/circle/index.js
@@ -1,11 +1,12 @@
-export default () =>
-  <div className="circle col-fs-c">
-    <div style={{height: 200, width: 200}} />
-    <div style={{height: 200, width: 200}} />
-    <div style={{height: 200, width: 200}} />
-    <div style={{height: 200, width: 200}} />
-    <div style={{height: 200, width: 200}} />
-    <div style={{height: 200, width: 200}} />
-    <div style={{height: 200, width: 200}} />
+import CircleHost from './components/circlehost';
 
+export default () =>
+  <div className="circle row-sb">
+    <div className="circle__side col">
+      <CircleHost />
+    </div>
+    <div className="circle__cent col">
+      <span>Testy Testers</span>
+    </div>
+    <div className="circle__side col"></div>
   </div>
