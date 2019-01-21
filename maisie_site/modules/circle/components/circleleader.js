ac1083c2206c3cafea2e3bbd291b0dfa35664dae
mkochakian <mattk4@me.com>
2019-01-21 14:13:53 -0500

    added a component to display circle host

---
+++ maisie_site/modules/circle/components/circleleader.js
@@ -0,0 +1,10 @@
+export default () =>
+  <div className="circleleader col-fs-c">
+    <span className="circle__title">Circle Leader</span>
+    <div className="circleleader__inner col-fs-c">
+      <div className="circleleader__inner-imgc col-c-c"><img src="../../../static/shared/matthew.png" /></div>
+      <span className="circleleader__inner-title">Hestor Chzburger</span>
+      <span className="circleleader__inner-desc">XYZ</span>
+      <div className="circleleader__inner-cta col-c-c">Book 1:1 time with Host</div>
+    </div>
+  </div>
