46b76e043f4e781f7ee1b6ee955c9410a62dc01d
mkochakian <mattk4@me.com>
2019-01-22 18:35:49 -0500

    discover

--- maisie_site/modules/discover/components/featured_groups/components/buttons.js
+++ maisie_site/modules/discover/components/featured_groups/components/buttons.js
@@ -1,3 +1,9 @@
 export default () =>
   <div className="featured_groups__buttons row-sb-c">
+    <div className="col-c-c">
+      {"<"}
+    </div>
+    <div className="col-c-c">
+      {">"}
+    </div>
   </div>
