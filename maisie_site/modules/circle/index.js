c23182b31ea96f21027ecaae6158571aba64213d
mkochakian <mattk4@me.com>
2019-01-23 17:20:54 -0500

    working on listing

--- maisie_site/modules/circle/index.js
+++ maisie_site/modules/circle/index.js
@@ -2,12 +2,38 @@ import CircleHost from './components/circlehost';
 import MeetingTime from './components/meetingtime';
 
 export default () =>
-  <div className="circle row-sb">
-    <div className="circle__cent col">
-      <span>Testy Testers</span>
-    </div>
-    <div className="circle__side col">
-      <CircleHost />
-      <MeetingTime />
-    </div>
+  <div>
+
   </div>
+
+
+// GENERAL STUFF
+//   circle name
+//   host name + qualification
+//   description
+//
+// CATEGORICAL STUFF
+//   tags (categories)
+//   "goals" (ie: "clear your mind", "solve problems", "build relationships")
+//
+// LOGISTICAL STUFF
+//   price
+//   meeting time
+//   head count + cap
+//   address / neighborhood
+//   location type (shared, office, community center)
+//   number of sessions
+//   cancellation / absence policy
+//   new members policy
+
+
+// export default () =>
+//   <div className="circle row-sb">
+//     <div className="circle__cent col">
+//       <span>Testy Testers</span>
+//     </div>
+//     <div className="circle__side col">
+//       <CircleHost />
+//       <MeetingTime />
+//     </div>
+//   </div>
