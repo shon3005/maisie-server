9f608b72da98443eeb2a6f707234acab884c19c9
Shaun Chua <shaunc@vikartech.com>
2019-01-24 05:32:25 -0500

    reference to s3 bucket favicon

--- maisie_api/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mix ecto.create
-mix ecto.migrate
-# mix run priv/repo/seeds.exs
- 
-exec "$@"
--- maisie_site/pages/_app.js
+++ maisie_site/pages/_app.js
@@ -17,14 +17,14 @@ class MyApp extends App {
           <meta name="description" content="Affordable mental health care" />
           <meta name="keywords" content="maisie, mental, health, community, counseling, group, groups, therapy, HTML, CSS, XML, JavaScript" />
           <meta name="author" content="Maisie, Inc." />
-          <link rel="apple-touch-icon" sizes="152x152" href="/static/shared/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/shared/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/shared/favicon-16x16.png" />
-          <link rel="manifest" href="/static/shared/site.webmanifest" />
-          <link rel="mask-icon" href="/static/shared/safari-pinned-tab.svg" color="#7692ff" />
-          <link rel="shortcut icon" href="/static/shared/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="152x152" href="https://s3.amazonaws.com/maisie-files/shared/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="https://s3.amazonaws.com/maisie-files/shared/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="https://s3.amazonaws.com/maisie-files/shared/favicon-16x16.png" />
+          <link rel="manifest" href="https://s3.amazonaws.com/maisie-files/shared/site.webmanifest" />
+          <link rel="mask-icon" href="https://s3.amazonaws.com/maisie-files/shared/safari-pinned-tab.svg" color="#7692ff" />
+          <link rel="shortcut icon" href="https://s3.amazonaws.com/maisie-files/shared/favicon.ico" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/static/shared/browserconfig.xml" />
+          <meta name="msapplication-config" content="https://s3.amazonaws.com/maisie-files/shared/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <ApolloProvider client={apolloClient}>
