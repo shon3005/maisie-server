80309746609b0f87df2cbd2f46a9fb79dd869d2a
mkochakian <mattk4@me.com>
2019-01-24 16:44:31 -0500

    updates to data

--- maisie_site/modules/circle/dummy_data.js
+++ maisie_site/modules/circle/dummy_data.js
@@ -1,19 +1,20 @@
 const DUMMY_DATA = [
   {
     "title": "Weekly Walk and Talk",
-    "thumbnail": "",
     "tags": ["active", "clear your mind", "early morning", "outdoors"],
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "price": 45,
-    "day": "monday",
-    "time": 2000,
-    "duration": 8,
-    "start": "",
-    "space_type": 2,
-    "neighborhood": "williamsburg",
+    "price": 45, // price of each session
+    "day": "monday", // day of each session
+    "time": 2000, // time of session start
+    "duration": 60, // length of each session
+    "program_length": 8, // number of weeks
+    "start": "", // date of program start (end is calculated automatically)
+    "space_type": "outdoors", // outdoors / shared / private -> describes type of space at which meetings are held
+    "neighborhood": "williamsburg", // location, needs to be amended with specific address later
     "host": {
       "name": "chester chzburger",
-      "license": "lcsw",
+      "thumbnail": "", // host profile pic
+      "license": "lcsw", // licensure (qualifications)
       "education": "new york university",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     }
