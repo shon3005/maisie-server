69212d159936f8c3cee8e2438bc1b7b71329acfd
mkochakian <mattk4@me.com>
2019-01-24 16:37:29 -0500

    dummy data

---
+++ maisie_site/modules/circle/dummy_data.js
@@ -0,0 +1,22 @@
+const DUMMY_DATA = [
+  {
+    "title": "Weekly Walk and Talk",
+    "thumbnail": "",
+    "tags": ["active", "clear your mind", "early morning", "outdoors"],
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "price": 45,
+    "day": "monday",
+    "time": 2000,
+    "duration": 8,
+    "start": "",
+    "space_type": 2,
+    "neighborhood": "williamsburg",
+    "host": {
+      "name": "chester chzburger",
+      "license": "lcsw",
+      "education": "new york university",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    }
+  }
+]
+export default DUMMY_DATA;
