50811a7686906c781f1e3982e9c02387cf873434
mkochakian <mattk4@me.com>
2019-01-28 23:45:14 -0500

    home page stuff

--- maisie_site/modules/home/components/featured_groups/components/buttons.js
+++ maisie_site/modules/home/components/featured_groups/components/buttons.js
@@ -1,9 +1,11 @@
 export default () =>
-  <div className="featured_groups__buttons row-sb-c">
-    <div className="col-c-c">
-      {"<"}
-    </div>
-    <div className="col-c-c">
-      {">"}
+  <div className="featured_groups__buttons row-c-c">
+    <div className="row-sb-c">
+      <div className="col-c-c">
+        {"<"}
+      </div>
+      <div className="col-c-c">
+        {">"}
+      </div>
     </div>
   </div>
--- maisie_site/modules/home/components/featured_groups/index.js
+++ maisie_site/modules/home/components/featured_groups/index.js
@@ -1,9 +1,20 @@
 import Dots from './components/dots';
 import Buttons from './components/buttons';
 
+const Choices = (props) => props.children.map((x, index) =>
+  <div className="featured_groups__choices-choice col-c-c">
+    <a href={x[1]}>{x[0]}</a>
+    <div />
+  </div>
+)
+// <Dots />
+
 export default () =>
   <div className="featured_groups col-c-c">
     <Buttons />
-    <Dots />
-    <img className="featured_groups__imgfeat" src="../../../../static/landing/background.jpg" />
+    <div className="featured_groups__choices row-sb-c">
+    </div>
+    <div className="featured_groups__imgfeat"><img src="https://s3.amazonaws.com/maisie-files/landing/back.png" /></div>
   </div>
+
+  // <Choices>{[["Stress", "#"], ["Confidence", "#"], ["Activities", "#"], ["Choice 4", "#"]]}</Choices>
