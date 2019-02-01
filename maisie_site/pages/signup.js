a305e04f6aa813ec88d42c5b5dc268eb41255452
mkochakian <mattk4@me.com>
2019-02-01 18:27:34 -0500

    connecting signup page

--- maisie_site/pages/signup.js
+++ maisie_site/pages/signup.js
@@ -1,56 +1,147 @@
+import React, { Component } from "react";
+var classNames = require('classnames');
+
 const Error = (props) => <div className="signin__error">{props.children}</div>
 
-export default () =>
-  <div className="signupfull col-c-c">
-    <div className="signupfull__inner row-c-c">
+// functions to check for valid entries... replace logic with backend calls if needed
+function email_check(x) {
+  var filter = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+  return String(x).search (filter) != -1;
+}
+function firstname_check(x) {
+  return x.length > 0 && !parseInt(x)
+}
+function lastname_check(x) {
+  // insert logic
+  return x.length > 0 && !parseInt(x)
+}
+function pass_check(x) {
+  // insert logic
+  return x.length >= 6
+}
 
-      <div className="signupfull__inner_right col-c-c">
-        <img src="../static/shared/newlogo.svg"/>
-                      <div style={{height: 20}} />
-        <span className="signupfull__inner_right-welcome">welcome to maisie</span>
-        <span className="signupfull__inner_right-subwelcome">create an account</span>
-                      <div style={{height: 20}} />
-        <form className="col-fs-c">
-          <input
-            required
-            placeholder="email address"
-            type="text"
-          />
-                      <div style={{height: 10}} />
-          <input
-            required
-            placeholder="password"
-            type="password"
-          />
-                      <div style={{height: 10}} />
-          <input
-          required
-          placeholder="confirm password"
-          type="password"
-          />
-          <div style={{height: 10}} />
-          <div className="row">
-            <input
-              required
-              placeholder="first name"
-              type="text"
-            />
-            <div style={{width: 10}} />
-            <input
-              required
-              placeholder="last name"
-              type="text"
-            />
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allChecked: false,
+      $email: '',
+      $firstName: '',
+      $lastName: '',
+      $password: '',
+      $passwordConfirmation: '',
+    }
+  }
+  listenForFilledOutFields() {
+    return this.state.email === "success" && this.state.firstname === "success" && this.state.lastname === "success" && this.state.password === "success" && this.state.confirm === "success" && this.state.zip === "success"
+  }
+  confirm_check(x) {
+    return x === this.refs.password.value
+  }
+  handleBlur(e, x) {
+    let f
+    if (x === "email") { f = email_check(e) }
+    else if (x === "firstname") { f = firstname_check(e) }
+    else if (x === "lastname") { f = lastname_check(e) }
+    else if (x === "password") { f = pass_check(e) }
+    else if (x === "confirm") { f = this.confirm_check(e) }
+    this.setState({ [x]: f ? "success" : "failure" })
+  }
+  render() {
+    return(
+      <div className="signupfull col-c-c">
+        <div className="signupfull__inner row-c-c">
+
+          <div className="signupfull__inner_right col-c-c">
+            <img src="../static/shared/newlogo.svg"/>
+                          <div style={{height: 20}} />
+            <span className="signupfull__inner_right-welcome">welcome to maisie</span>
+            <span className="signupfull__inner_right-subwelcome">create an account</span>
+                          <div style={{height: 20}} />
+            <form className="col-fs-c" onSubmit={(e) => {
+              this.props.onSubmit({
+                firstName: this.state.$firstName,
+                lastName: this.state.$lastName,
+                email: this.state.$email,
+                password: this.state.$password,
+                passwordConfirmation: this.state.$passwordConfirmation,
+              })}}>
+              <input
+                className={classNames({"fieldSuccess": this.state.email === "success", "fieldFailure": this.state.email === "failure"})}
+                required
+                onChange={(e, x) => {
+                  this.handleBlur(e.target.value, "email");
+                  this.setState({ $email: e.target.value });
+                }}
+                name="email"
+                placeholder="email address"
+                type="text"
+              />
+              {/*<div className={classNames({"signupfull_error": this.props.errors.email })}>INVALID EMAIL</div>*/}
+                          <div style={{height: 10}} />
+              <input
+                className={classNames({"fieldSuccess": this.state.password === "success", "fieldFailure": this.state.password === "failure"})}
+                required
+                onChange={(e, x) => {
+                  this.handleBlur(e.target.value, "password");
+                  this.setState({ $password: e.target.value });
+                }}
+                name="password"
+                placeholder="password"
+                type="password"
+                ref="password"
+              />
+                          <div style={{height: 10}} />
+              <input
+                className={classNames({"fieldSuccess": this.state.confirm === "success", "fieldFailure": this.state.confirm === "failure"})}
+                required
+                onChange={(e, x) => {
+                  this.handleBlur(e.target.value, "confirm");
+                  this.setState({ $passwordConfirmation: e.target.value });
+                }}
+                name="confirm"
+                placeholder="confirm password"
+                type="password"
+              />
+              <div style={{height: 10}} />
+              <div className="row">
+                <input
+                  className={classNames({"fieldSuccess": this.state.firstname === "success", "fieldFailure": this.state.firstname === "failure"})}
+                  onChange={(e, x) => {
+                    this.handleBlur(e.target.value, "firstname");
+                    this.setState({ $firstName: e.target.value });
+                  }}
+                  name="firstname"
+                  required
+                  placeholder="first name"
+                  type="text"
+                />
+                <div style={{width: 10}} />
+                <input
+                  className={classNames({"fieldSuccess": this.state.lastname === "success", "fieldFailure": this.state.lastname === "failure"})}
+                  onChange={(e, x) => {
+                    this.handleBlur(e.target.value, "lastname");
+                    this.setState({ $lastname: e.target.value });
+                  }}
+                  name="lastname"
+                  required
+                  placeholder="last name"
+                  type="text"
+                />
+              </div>
+                          <div style={{height: 20}} />
+              <button type="submit">
+                Sign Up
+              </button>
+            </form>
+                          <div style={{height: 20}} />
+            <a className="signuplink" href="/signin">Already have an account?</a>
+
+            <Error></Error>
           </div>
-                      <div style={{height: 20}} />
-          <button type="submit">
-            Sign Up
-          </button>
-        </form>
-                      <div style={{height: 20}} />
-        <a className="signuplink" href="/signin">Already have an account?</a>
-
-        <Error></Error>
+        </div>
       </div>
-    </div>
-  </div>
+    )
+  }
+
+}
