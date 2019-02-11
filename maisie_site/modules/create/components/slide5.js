7b4d76a6e4a867df2e8470812fbca7ef725bd9ea
mkochakian <mattk4@me.com>
2019-02-10 19:19:51 -0500

    making the review page look better

--- maisie_site/modules/create/components/slide5.js
+++ maisie_site/modules/create/components/slide5.js
@@ -21,17 +21,17 @@ export default (props) =>
     <div className="create__inner_cont">
       <LargeText>Review</LargeText>
       <SmallText>Is everything correct?</SmallText>
-      <Field title="Title"><span>{props.title}</span></Field>
-      <Field title="Description"><span>{props.description}</span></Field>
+      <Field title="Title"><span className="create__field_input-textbox">{props.title}</span></Field>
+      <Field title="Description"><span className="create__field_input-textbox">{props.description}</span></Field>
       <Divider />
-      <Field title="Day"><span style={{textTransform: "capitalize"}}>{props.day}</span></Field>
-      <Field title="Frequency"><span style={{textTransform: "capitalize"}}>{props.frequency}</span></Field>
-      <Field title="Length"><span style={{textTransform: "capitalize"}}>{props.length}</span></Field>
-      <Field title="Time"><span>{props.hour + ":" + props.minute + " " + props.ampm}</span></Field>
+      <Field title="Day"><span className="create__field_input-textbox" style={{textTransform: "capitalize"}}>{props.day}</span></Field>
+      <Field title="Frequency"><span className="create__field_input-textbox" style={{textTransform: "capitalize"}}>{props.frequency}</span></Field>
+      <Field title="Length"><span className="create__field_input-textbox" style={{textTransform: "capitalize"}}>{props.length}</span></Field>
+      <Field title="Time"><span className="create__field_input-textbox">{props.hour + ":" + props.minute + " " + props.ampm}</span></Field>
       <Divider />
-      <Field title="Location Type"><span style={{textTransform: "capitalize"}}>{props.location_type}</span></Field>
-      <Field title="Neighborhood"><span style={{textTransform: "capitalize"}}>{props.neighborhood}</span></Field>
-      <Field title="Address"><span style={{textTransform: "capitalize"}}>{props.address}</span></Field>
+      <Field title="Location Type"><span className="create__field_input-textbox" style={{textTransform: "capitalize"}}>{props.location_type}</span></Field>
+      <Field title="Neighborhood"><span className="create__field_input-textbox" style={{textTransform: "capitalize"}}>{props.neighborhood}</span></Field>
+      <Field private={true} title="Address"><span className="create__field_input-textbox" style={{textTransform: "capitalize"}}>{props.address}</span></Field>
       <Divider />
-      <Field title="Price"><span>{"$" + props.price}</span></Field>
+      <Field title="Price"><span className="create__field_input-textbox">{"$" + props.price}</span></Field>
     </div>
