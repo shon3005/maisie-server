729c748a46cd5de551eef46ccfd09de02aea7685
mkochakian <mattk4@me.com>
2019-02-12 11:09:19 -0800

    slight tweaks

--- maisie_site/modules/home/components/card.js
+++ maisie_site/modules/home/components/card.js
@@ -1,5 +1,5 @@
 export default (props) =>
-  <a className="homecard col-fs-c" href="circles/001">
+  <a className="homecard col-fs-c" href="circle/001">
     <div className="homecard-img col-c-c">
       <img src="../../../static/shared/matthew.png" />
     </div>
