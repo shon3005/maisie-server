fe1dec4a293b01602c85649edda6a182652f9373
mkochakian <mattk4@me.com>
2019-02-12 22:47:45 -0800

    added host

--- maisie_site/modules/circle/components/left/components/hosted.js
+++ maisie_site/modules/circle/components/left/components/hosted.js
@@ -1,2 +1,12 @@
 export default (props) =>
+
+
+<div className="hostabb row">
+  <div className="hostabb__thumb"><img src={props.img} /></div>
+  <div className="hostabb__text col">
+    <span className="hostabb__text-tag">hosted by</span>
+    <span className="hostabb__text-name">{props.name}</span>
+  </div>
+</div>
+
   <div />
