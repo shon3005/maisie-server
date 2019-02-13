f60d8b016aab166d9016cea69e898b6c3539fc61
mkochakian <mattk4@me.com>
2019-02-12 21:56:23 -0800

    new circle page initialized

--- maisie_site/modules/circle/dummy_data.js
+++ maisie_site/modules/circle/dummy_data.js
@@ -1,6 +1,7 @@
 const DUMMY_DATA =
   {
     "title": "Weekly Walk and Talk",
+    "img": "../../static/shared/example_background.png",
     "tags": ["active", "clear your mind", "early morning", "outdoors"],
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     "price": 45.00, // price of each session
@@ -8,6 +9,8 @@ const DUMMY_DATA =
     "time": 2000, // time of session start
     "duration": 60, // length of each session
     "program_length": 8, // number of weeks
+    "needed_to_start": 5,
+    "num_joined": 3,
     "start": "2019-02-05T20:00:00-05:00", // date of program start (end is calculated automatically)
     "space_type": "outdoors", // outdoors / shared / private -> describes type of space at which meetings are held
     "neighborhood": "williamsburg", // location, needs to be amended with specific address later
