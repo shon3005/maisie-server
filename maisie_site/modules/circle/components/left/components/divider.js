37a8544a4f0eeb9aae92a62dedcd278a4daddff9
mkochakian <mattk4@me.com>
2019-02-12 23:10:15 -0800

    ~

---
+++ maisie_site/modules/circle/components/left/components/divider.js
@@ -0,0 +1,10 @@
+export default () =>
+  <div
+    style={{
+      height: 2,
+      width: '100%',
+      backgroundColor: 'rgba(5,45,84,.05)',
+      marginTop: 20,
+      marginBottom: 20,
+    }}
+  />
--- maisie_site/modules/circle/components/left/index.js
+++ maisie_site/modules/circle/components/left/index.js
@@ -1,10 +1,12 @@
 import LargeText from '../../../../shared/components/text/largeText.js';
 import Hosted from './components/hosted.js';
+import Divider from './components/divider.js';
 
 export default (props) =>
   <div className="circle_left">
     <LargeText>{props.d.title}</LargeText>
     <div style={{height: 10}} />
     <Hosted host={props.d.host} abb />
+    <Divider />
     <Hosted host={props.d.host} />
   </div>
