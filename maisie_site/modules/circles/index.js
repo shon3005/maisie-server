16e9a95d47be42abfedac9df045aceee6470f592
mkochakian <mattk4@me.com>
2019-02-12 16:43:55 -0800

    tweaks

--- maisie_site/modules/circles/index.js
+++ maisie_site/modules/circles/index.js
@@ -1,4 +1,4 @@
 import Blank from './components/blank.js';
 import Circles from './components/circles/index.js';
 
-export default (props) => props.blank ? <Blank /> : <Circles requests={2} />
+export default (props) => props.blank ? <Blank /> : <Circles requests={5} />
