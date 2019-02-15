f8cd3288c88a5d26fe0715441286637792e6c9f6
mkochakian <mattk4@me.com>
2019-02-14 22:31:54 -0800

    ~

--- maisie_site/pages/messages.js
+++
@@ -1,4 +0,0 @@
-import withPageNoSide from '../shared/withPageNoSide.js';
-import Messages from '../modules/messages/index.js';
-
-export default () => withPageNoSide(<Messages />, "messages");
--- maisie_site/pages/schedule.js
+++
@@ -1,4 +0,0 @@
-import withPageNoSide from '../shared/withPageNoSide.js';
-import Schedule from '../modules/schedule/index.js';
-
-export default () => withPageNoSide(<Schedule />, "schedule");
