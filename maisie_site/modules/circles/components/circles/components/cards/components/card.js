dd777326f223485bcff3752ee38d03d18d1d9a46
mkochakian <mattk4@me.com>
2019-02-14 19:26:33 -0800

    tweaks

--- maisie_site/modules/circles/components/circles/components/cards/components/card.js
+++ maisie_site/modules/circles/components/circles/components/cards/components/card.js
@@ -10,17 +10,20 @@ export default (props) =>
         <span className="mycirclescard__cont_text-eco"><span className="price">{"$ " + props.data.price}</span><span>{" • " + props.data.min + " people minimum"}</span></span>
       </div>
     </div>
-    <div className="row-fe-c">
-      <div className={classNames(["mycirclescard-pending", {
-        pending: props.data.pending,
-      }])}>
-        <div>{ props.data.pending === true ? "Pending" : null }</div>
+    <div className="mycirclescard__right col-fs-fe">
+      <div className="row-fe-c">
+        <div className={classNames(["mycirclescard-pending", {
+          pending: props.data.pending,
+        }])}>
+          <div>{ props.data.pending === true ? "Pending" : null }</div>
+        </div>
+        <div className={classNames(["mycirclescard-pending", {
+          host: props.data.status === "host",
+          member: props.data.status === "member",
+        }])}>
+          <div>{ props.data.status === "host" ? "Host" : props.data.status === "member" ? "Member" : null }</div>
+        </div>
       </div>
-      <div className={classNames(["mycirclescard-pending", {
-        host: props.data.status === "host",
-        member: props.data.status === "member",
-      }])}>
-        <div>{ props.data.status === "host" ? "Host" : props.data.status === "member" ? "Member" : null }</div>
-      </div>
+      <div className="mycirclescard__right-pay"><em>{props.data.status === "member" ? "Last payment: 2/07/19" : null}</em></div>
     </div>
   </a>
