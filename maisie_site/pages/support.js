c2dc7984b51a69a5ccf145478941a3ddaa87873c
Shaun Chua <sc4224@nyu.edu>
2019-02-14 23:01:33 -0800

    import connect

--- maisie_site/pages/support.js
+++ maisie_site/pages/support.js
@@ -1,6 +1,7 @@
 import SupportModule from '../modules/support/index.js';
 import Header from '../shared/components/header/index.js';
 import Footer from '../shared/components/footer.js';
+import { connect } from 'react-redux';
 
 const Support = () =>
 <div className="support col-fs-c">
