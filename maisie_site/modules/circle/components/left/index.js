a0d939bfc8517367b36935a50946f6a68e694126
mkochakian <mattk4@me.com>
2019-02-19 14:39:22 -0800

    improvements to circle page

--- maisie_site/modules/circle/components/left/index.js
+++ maisie_site/modules/circle/components/left/index.js
@@ -4,12 +4,15 @@ import Divider from './components/divider.js';
 import Tags from './components/tags.js';
 import Details from './components/details.js';
 import Calendar from './components/calendar/index.js';
+import Location from './components/location.js';
+
 
 const Sub = (props) => <div className="circle_left__subhead">{props.children}</div>
 var classNames = require('classnames')
 export default (props) => {
   const day = props.circle && props.circle.day ? props.circle.day : 'tuesday';
   const minute = props.circle && props.circle.minute ? props.circle.minute : 0;
+  const location = props.circle && props.circle.locationType ? props.circle.locationType : "private space";
   let hour = props.circle && props.circle.hour ? props.circle.hour : 0;
   hour = props.circle && props.circle.ampm && props.circle.ampm === 'am' ?
     hour :
@@ -20,12 +23,12 @@ export default (props) => {
   return (
     <div className={classNames(["circle_left", {"dark_theme": props.dark}])}>
       <LargeText>{props.circle ? props.circle.title : null}</LargeText>
-      <div style={{height: 10}} />
+      <div style={{height: 20}} />
       <Hosted host={{
         name: props.user.firstName + ' ' + props.user.lastName,
         imageUrl: props.user.host.imageUrl
       }} abb />
-      <div style={{height: 10}} />
+      <div style={{height: 20}} />
       <div className="circle_left_tags row">
         <Tags tags={props.d.tags} />
       </div>
@@ -38,6 +41,12 @@ export default (props) => {
       <Divider />
       <Sub>who should join this circle?</Sub>
       <div className="dark_theme_primary_text circle_left-text">{props.d.who_should_join}</div>
+      <Divider />
+
+      <Sub>location</Sub>
+      <div style={{height: 20}} />
+      <Location neighborhood={props.circle.neighborhood} type={location} />
+      <div style={{height: 20}} />
       <Divider />
       <Sub>about the host</Sub>
       <Hosted host={{
