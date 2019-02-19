70d8d0359430fd1f678bc31241b5897887eb5cfd
mkochakian <mattk4@me.com>
2019-02-18 17:01:05 -0800

    got rid of pages we have no use for rn

--- maisie_site/modules/panel/hostheader/index.js
+++ maisie_site/modules/panel/hostheader/index.js
@@ -4,12 +4,12 @@ var classNames = require('classnames')
 export default (props) =>
   <div className="hostheader row-c-c">
     <div className="row-fs-c">
-      <Link as="/panel" href="/panel/dash">
+      {/*<Link as="/panel" href="/panel/dash">
         <div className={classNames(["col-c-c", {"selected": props.page === "dash"}])}>
           <img src="../../../static/shared/house.svg" />
           <div />
         </div>
-      </Link>
+      </Link>*/}
       <Link as="/panel/finances" href="/panel/finances">
         <div className={classNames(["col-c-c", {"selected": props.page === "finances"}])}>
           <span>Finances</span>
