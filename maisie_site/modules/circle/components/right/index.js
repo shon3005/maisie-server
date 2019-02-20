615179c03be6aa3818e605fd857292da62dbc058
mkochakian <mattk4@me.com>
2019-02-19 16:19:12 -0800

    fixing more things

--- maisie_site/modules/circle/components/right/index.js
+++ maisie_site/modules/circle/components/right/index.js
@@ -1,6 +1,7 @@
 import Socials from './components/socials.js';
 import Dots from './components/dots.js';
-
+import Divider from './components/divider.js';
+import Button from '../../../../shared/components/button.js';
 const Sub = (props) => <div className="circle_left__subhead">{props.children}</div>
 var classNames= require('classnames')
 function handleClick() {
@@ -14,26 +15,23 @@ export default (props) => {
         <div className="circle_right__inner_cont-price col-fs-c dark_theme_primary_text">
           <span><span className="large">{"$" + props.circle.price}</span> per session</span>
         </div>
-        <div style={{height: 20}} />
-        <div className="dark_theme_dark_back circle_right__inner-joined col-c-c">
-          <span className="dark_theme_tag_text">{props.d.num_joined + " joined (" + (props.circle.min - props.d.num_joined) + " more needed)"}</span>
-          <div className="row-sa-c">
-            <Dots d={props.d} />
-          </div>
+        <div style={{height: 10}} />
+        <div className="circle_right__inner_cont-price col-fs-c dark_theme_primary_text">
+          <span className="lower">{props.circle.min + "-" + props.circle.max + " members"}</span>
         </div>
         <div style={{height: 25}} />
         <div className="circle_right__inner_cont-join">
-          <div className={classNames({
+          <Button className={classNames({
             "requested": props.status === "requested",
             "joined": props.status === "joined",
-            "col-c-c": true,
-          })}>{
+            "col-c-c": true, })}
+          >{
             props.status === "requested"
               ? "Requested"
               : props.status === "joined"
                 ? "Already joined"
                 : "Request to join"
-          }</div>
+          }</Button>
         </div>
         <div onClick={handleClick} className="circle_right__inner_cont-ask col-c-c">Ask a question</div>
       </div>
