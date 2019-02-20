d5bc8f1ca1a1d72352e752eca8d39611ca88e66a
mkochakian <mattk4@me.com>
2019-02-19 16:02:50 -0800

    tiny fix

--- maisie_site/modules/circle/components/left/components/divider.js
+++ maisie_site/modules/circle/components/left/components/divider.js
@@ -4,7 +4,7 @@ export default () =>
       height: 2,
       width: '100%',
       backgroundColor: 'rgba(5,45,84,.05)',
-      marginTop: 20,
-      marginBottom: 20,
+      marginTop: 40,
+      marginBottom: 40,
     }}
   />
