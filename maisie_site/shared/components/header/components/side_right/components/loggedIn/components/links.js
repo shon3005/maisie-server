40211d29c7e4cfacd3399be697803bec4aaf30f4
mkochakian <mattk4@me.com>
2019-02-22 17:37:43 -0800

    header improvements

--- maisie_site/shared/components/header/components/side_right/components/loggedIn/components/links.js
+++
@@ -1,6 +0,0 @@
-import Item from './item.js';
-import links from '../utils/links.js';
-
-export default (props) => links.map((x, index) =>
-  <Item key={index} text={x.text} img={x.img} href={x.href} />
-)
