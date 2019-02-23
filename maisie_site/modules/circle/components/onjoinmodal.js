31e78a05864bd1436a7dd00c929850da1c3e8aed
mkochakian <mattk4@me.com>
2019-02-22 18:31:46 -0800

    fixed a formatting issue

--- maisie_site/modules/circle/components/onjoinmodal.js
+++ maisie_site/modules/circle/components/onjoinmodal.js
@@ -1,4 +1,4 @@
-import SmallText from '../../../shared/components/text/smallText.js';
+import LargeText from '../../../shared/components/text/largeText.js';
 import Button from '../../../shared/components/button.js';
 
 function handleClick() {
@@ -9,7 +9,8 @@ function handleClick() {
 export default (props) =>
   <div id="onjoinmodal" className="hide circle_askmodal col-c-c">
     <div className="col-c-c">
-      <SmallText>Request Sent.</SmallText>
+      <div className="row-c-c"><LargeText>Request Sent.</LargeText></div>
+      <div style={{height: 10}} />
       <div className="circle_askmodal-text">{props.user.firstName + " has been notified of your request"}<br />and will reach out with next steps.</div>
       <Button kind="alt" weight="dark" onClick={handleClick}>Okay</Button>
     </div>
