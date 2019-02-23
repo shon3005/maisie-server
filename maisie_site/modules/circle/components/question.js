8857cec219f0a635a64c3fb06e9ba5a36627d2a1
mkochakian <mattk4@me.com>
2019-02-23 00:33:36 -0800

    fixed a question bug

--- maisie_site/modules/circle/components/question.js
+++ maisie_site/modules/circle/components/question.js
@@ -4,21 +4,24 @@ import Button from '../../../shared/components/button.js';
 function handleClick() {
   // send question
   // // // insert code here
-  // close modal
-  document.getElementById("askmodal").classList.add('hide');
-  // erase contents
-  document.getElementById("askmodal_textarea").value = "";
+  document.getElementById("askmodal").classList.add('hide'); // close modal
+  document.getElementById("askmodal_textarea").value = ""; // erase contents
+}
+
+function handleChange() {
+  var val = document.getElementById("askmodal_textarea").value; // get value of textarea
+  val ? document.getElementById("askmodal_sendbutton").classList.remove('fade') : document.getElementById("askmodal_sendbutton").classList.add('fade'); // if changes, allow submit button
 }
 
 export default () =>
   <div id="askmodal" className="hide circle_askmodal col-c-c">
     <div className="col">
       <SmallText>Ask this host a question</SmallText>
-      <textarea id="askmodal_textarea" rows={6} placeholder="Type your question here...">
+      <textarea onChange={handleChange} id="askmodal_textarea" rows={6} placeholder="Type your question here...">
       </textarea>
       <div className="row-fe">
         <Button kind="alt" weight="light" onClick={handleClick}>Cancel</Button>
-        <Button kind="alt" weight="purple" onClick={handleClick}>Send</Button>
+        <Button id="askmodal_sendbutton" className="fade" kind="alt" weight="purple" onClick={handleClick}>Send</Button>
       </div>
     </div>
   </div>
