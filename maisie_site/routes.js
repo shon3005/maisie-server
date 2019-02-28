05df912f60fa3b213d9405544829e5f28f8a3fa4
Shaun Chua <sc4224@nyu.edu>
2019-02-28 15:28:53 +0800

    edit routes file

--- maisie_site/routes.js
+++ maisie_site/routes.js
@@ -12,5 +12,5 @@ module.exports = routes()                           // ----   ----   -----      
 .add('create')
 .add('signup')
 .add('signin')
-.add('profile', '/profile/:id')
+.add('profile', '/profile/:id', 'profile')
 .add('editprofile')
