56a71e2c91b09e75cd5f96757fded800552b88db
Shaun Chua <sc4224@nyu.edu>
2019-03-07 01:39:55 +0800

    stripe public key config

--- maisie_site/next.config.js
+++ maisie_site/next.config.js
@@ -1,4 +1,8 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
 const withFonts = require('next-fonts')
-module.exports = withFonts(withSass({}))
+module.exports = withFonts(withSass({
+  publicRuntimeConfig: { // Will be available on both server and client
+    stripePublicKey: process.env.STRIPE_PUBLIC_KEY // Pass through env variables
+  }
+}))
