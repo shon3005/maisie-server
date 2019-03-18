6d22de0c1870ce6a7dd761a8e409a6ca2abefc1a
mkochakian <mattk4@me.com>
2019-03-17 22:53:37 -0700

    about page and a few changes

---
+++ maisie_site/pages/about.js
@@ -0,0 +1,82 @@
+import Header from '../shared/components/header/index.js';
+import Footer from '../shared/components/footer/index.js';
+import Button from '../shared/components/button.js';
+import { connect } from 'react-redux';
+
+const About = (props) =>
+  <div className="about">
+    <Header trnsp loggedIn={props.user ? 'loggedIn' : 'loggedOut'} />
+    <div className="about__in">
+      <h3>Why Maisie?</h3>
+      <p>
+        In 2018, we began working on Maisie as a way to make mental wellness more
+        accessible and open to all. Today, the average price for a therapy session
+        in cities like New York and San Francisco is more than $150. This is far, far too
+        high for most people. Yet at the same time, the average salary of a mental health
+        professional is less than $50,000 a year. Lower the price, and you cut salaries.
+        Raise salaries and you raise prices. And right now, neither side can budge.
+      </p>
+      <p>
+        But that's not all. In the US, the percent of people with some kind of mental health
+        condition is well over 20% (around 70 million people). And there are millions
+        more who will seek out therapy even without some kind of condition. However, there are
+        only around 500,000 mental health professionals. This means that the ratio of patients
+        to available providers in this country is nearly 140:1, more than four times the number
+        of patients any therapist will see at one time.
+      </p>
+      <p>
+        Our answer to these problems is group therapy. Trusted by many professionals, group therapy
+        is significantly more affordable than traditional 1:1 care and incredibly effective. Best of
+        all, in addition to a licensed professional leading the conversation, you also get to work with
+        great people, often working toward the same goals as you.
+      </p>
+      <p>
+        Maisie is the first site of its kind, and we are committed to making group therapy a widely available
+        and accessible form of mental wellness. If you're passionate about this mission, we'd love to hear from you.
+      </p>
+      <Button kind="link">Get in touch with us</Button>
+      <div style={{
+        height: 1,
+        width: "100%",
+        backgroundColor: "rgba(5, 45, 84, 0.05)",
+        marginTop: 50,
+        marginBottom: 50
+      }} />
+      <h3>Our Team</h3>
+      <div className="row-sb-c">
+        <div className="about_bio col-fs-c">
+          <div className="about_bio-img" style={{
+            backgroundImage: "url('../static/shared/matthew.png')",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }} />
+          <p>Matthew Kochakian</p>
+          <span>Co-founder</span>
+          <span className="social"><a href="https://twitter.com/kochakian">Twitter</a> | <a href="https://www.linkedin.com/in/mkochakian/">Linkedin</a></span>
+        </div>
+        <div style={{width: 30}} />
+        <div className="about_bio col-fs-c">
+          <div className="about_bio-img" style={{
+            backgroundImage: "url('../static/shared/shaun.png')",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }} />
+          <p>Shaun Chua</p>
+          <span>Co-founder</span>
+          <span className="social"><a href="https://twitter.com/shon3005">Twitter</a> | <a href="https://www.linkedin.com/in/shon3005/">Linkedin</a></span>
+        </div>
+      </div>
+      <div className="col-fs-c" style={{
+        marginTop: 50
+      }}>
+        <Button kind="primary" weight="purple" link="https://angel.co/maisie-1">Want to join our team?</Button>
+      </div>
+    </div>
+    <Footer />
+  </div>
+
+const mapStateToProps = (state) => {
+  return { user: state.user.user };
+}
+
+export default connect(mapStateToProps)(About);
